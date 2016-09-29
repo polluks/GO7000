920b1afa282b4bf4abe8c53874c7ab6ab6983170
Stefan <stefan.haubenthal@gmail.com>
2016-09-29 22:45:07 +0100

    Fixed jmp

--- 7.c
+++ 7.c
@@ -1,19 +1,22 @@
-/* GO7000
+/* $Id$
+ * GO7000
  * Dedicated to Stefan Egger
  */
 
 #include <stdio.h>
 
 extern char mem[];
-int pc;
+unsigned int pc;
 char opcode;
+char reg[8];
 
-const char VER[]="$ver: go7000 0.1 ("__DATE__") $";
+const char VER[]="$ver: go7000 0.2 ("__DATE__") $";
 
 void decode()
 {
 switch(opcode)
 	{
+	/* jmp */
 	case '\x04':
 	case '\x24':
 	case '\x44':
@@ -22,13 +25,38 @@ switch(opcode)
 	case '\xA4':
 	case '\xC4':
 	case '\xE4':
-	pc=(opcode&0xf0)<<3+mem[pc+1];
+	pc=((opcode&0xf0)<<3)+mem[pc+1];
+	break;
+
+	/* mov */
+	case '\xB8':
+	case '\xB9':
+	case '\xBA':
+	case '\xBB':
+	case '\xBC':
+	case '\xBD':
+	case '\xBE':
+	case '\xBF':
+	pc+=2;
+	break;
+
+	/* sel */
+	case '\xD5':
+	/* nop */
+	case '\x00':
+	++pc;
 	break;
 	}
 }
 
 void main()
 {
+opcode=mem[pc];
+printf("%x %x\n", opcode, pc);
+decode();
+opcode=mem[pc];
+printf("%x %x\n", opcode, pc);
+decode();
 opcode=mem[pc];
 printf("%x %x\n", opcode, pc);
 decode();
--- bins.s
+++ bins.s
@@ -1,3 +1,4 @@
 	.export	_mem
 _mem:	.incbin	"o2rom.bin"
-cart:	.incbin	"computer intro.bin"
+;cart:	.incbin	"computer intro.bin"
+cart:	.incbin	"Gunfighter (Europe).bin"
