f9553cf06867e51359e10d1dc04719ed09ebc5f6
polluks <polluks@sdf.org>
2026-06-29 10:58:08 +0200

    Add VIC colour constants to gpu.h
    Define VIC_BLACK through VIC_LTGREY as RGB triples for
    use in pixel rendering code.

--- gpu.h
+++ gpu.h
@@ -8,3 +8,20 @@ void *sid_addr();
 extern char vdc[256];
 extern char sram[32];
 extern char spr_y[4], spr_x[4], spr_attr[4];
+
+#define VIC_BLACK      0,0,0
+#define VIC_WHITE      255,255,255
+#define VIC_RED        136,0,0
+#define VIC_CYAN       170,255,238
+#define VIC_PURPLE     204,68,204
+#define VIC_GREEN      0,204,85
+#define VIC_BLUE       0,0,170
+#define VIC_YELLOW     238,238,119
+#define VIC_ORANGE     221,136,85
+#define VIC_BROWN      102,68,0
+#define VIC_PINK       255,119,119
+#define VIC_DARKGREY   51,51,51
+#define VIC_GREY       119,119,119
+#define VIC_LTGREEN    170,255,102
+#define VIC_LTBLUE     0,136,255
+#define VIC_LTGREY     187,187,187
